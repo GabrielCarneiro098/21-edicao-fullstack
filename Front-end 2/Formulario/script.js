b543b52d01cb69af77e32531041b8a4f9a9d1738
GabrielCarneiro098 <gabrielcarneiro098gc@gmail.com>
2024-10-20 19:38:00 -0300

    commit: Formulario

--- Front-end 2/Formulario/script.js
+++ Front-end 2/Formulario/script.js
@@ -1,30 +1,47 @@
-const email = document.querySelector("#email");
-const API = "https://api-recados2024.onrender.com/";
-const titulo = document.getElementById("title");
-const descricao = document.querySelector("#description");
-const enviar = document.querySelector("#enviar");
+const nome = document.getElementById("nome");
+const endereco = document.getElementById("endereco");
+const cidade = document.getElementById("cidade");
+const estado = document.getElementById("estado");
 
-function createNewNote(event) {
-  const recado = {};
-  axios
-    .post("https://api-recados2024.onrender.com/message", {
-      email: email.value,
-      title: titulo.value,
-      description: descricao.value,
-    })
-    .then((response) => {
-      console.log(response);
-      email.value = "";
-      titulo.value = "";
-      descricao.value = "";
-      alert(response.data.message);
-    })
-    .catch((error) => {
-      console.log(error);
-      alert("Verifique os campos antes de enviar");
-    });
+const curriculo = document.getElementById("curriculo");
 
+const enviar = document.getElementById("enviar");
+const limpar = document.getElementById("limpar");
+
+function enviarFomulario() {
   event.preventDefault();
+  if (nome.value == "" || endereco.value == "" || cidade.value == "") {
+    return alert("Preencha os campos corretamente.");
+  } else {
+    const interesses = Array.from(
+      document.querySelectorAll('input[name="interesse"]:checked')
+    );
+
+    console.log(interesses);
+    const dados = {
+      nome: nome.value,
+      endereço: endereco.value,
+      cidade: cidade.value,
+      estado: estado.value,
+      cargo: (cargo = document.querySelector(
+        'input[name="cargo"]:checked'
+      ).value),
+      interesse: interesses.map((checkbox) => checkbox.value),
+      mini_curriculo: curriculo.value,
+    };
+
+    console.log(dados);
+  }
 }
 
-enviar.addEventListener("click", createNewNote);
+function limparFormulario() {
+  nome.value = "";
+  endereco.value = "";
+  cidade.value = "";
+  document
+    .querySelectorAll('input[name="interesse"]')
+    .forEach((checkbox) => (checkbox.checked = false));
+}
+
+enviar.addEventListener("click", enviarFomulario);
+limpar.addEventListener("click", limparFormulario);
