c6fda1be8137cff5c60bc3bf4632d58b3a65c8b6
GabrielCarneiro098 <gabrielcarneiro098gc@gmail.com>
2024-11-21 20:49:12 -0300

    .

--- Front-end 3/Login e cadastro com bootstrap/script.js
+++ Front-end 3/Login e cadastro com bootstrap/script.js
@@ -2,10 +2,13 @@ const API = "https://api-recados2024.onrender.com";
 const nome = document.getElementById("nome");
 const email = document.getElementById("email");
 const senha = document.getElementById("senha");
+const requestMessage = document.getElementById("requestMessage");
+const response = document.getElementById("response");
 const cadastrar = document.getElementById("cadastrar");
 
-function teste() {
+function cadastro() {
   event.preventDefault();
+  requestMessage.style.display = "none";
 
   axios
     .post(`${API}/signup`, {
@@ -14,14 +17,17 @@ function teste() {
       senha: senha.value,
     })
     .then(function (response) {
+      alert(`Conta criada com sucesso!`);
       window.location.href = "./login.html";
       console.log(response);
     })
     .catch(function (error) {
-      console.log(error);
+      console.log(error.response.data);
+      response.innerText = error.response.data.error;
+      requestMessage.style.display = "block";
     });
 
   console.log(nome.value, email.value, senha.value);
 }
 
-cadastrar.addEventListener("click", teste);
+cadastrar.addEventListener("click", cadastro);
